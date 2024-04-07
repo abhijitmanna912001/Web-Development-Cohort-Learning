d252070055b2f402bb702c023c690b938b8983e7
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-04-07 14:28:26 +0530

    Program that greets a person based on gender using if else

---
+++ 2.js
@@ -0,0 +1,11 @@
+let firstName = "John";
+let lastName = "Doe";
+let gender = "female";
+
+if (gender === "male") {
+  console.log("Hello, Mr. " + lastName);
+} else if (gender === "female") {
+  console.log("Hello, Ms. " + lastName);
+} else {
+  console.log("Hello, " + firstName + " " + lastName);
+}
