0abdc51d7876064753209516384afbf93c4f44eb
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-04-07 13:05:45 +0530

    Program that greets a person using their first and last name

---
+++ 1.js
@@ -0,0 +1,8 @@
+let firstName = "Abhijit";
+let lastName = "Manna";
+
+if (firstName && lastName) {
+  console.log("Hello, " + firstName + " " + lastName + "!");
+} else {
+  console.log("Please provide both first and last names.");
+}
